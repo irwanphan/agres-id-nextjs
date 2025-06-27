5b9b15ad872c03f5e7ede591d57122389e5599a3
irwanphan <irwanphan@gmail.com>
2025-06-27 12:55:50 +0700

    Update logo.svg to a new design and change currency formatting in formatPrice.ts to Indonesian Rupiah (IDR)

--- src/utils/formatePrice.ts
+++ src/utils/formatePrice.ts
@@ -3,9 +3,16 @@
 export const formatPrice = (price: number) => {
     const hasDecimals = price % 1 !== 0;
   
-    return new Intl.NumberFormat("en-US", {
+    // return new Intl.NumberFormat("en-US", {
+    //   style: "currency",
+    //   currency: "USD",
+    //   minimumFractionDigits: hasDecimals ? 2 : 0,
+    //   maximumFractionDigits: hasDecimals ? 2 : 0,
+    // }).format(price);
+
+    return new Intl.NumberFormat("id-ID", {
       style: "currency",
-      currency: "USD",
+      currency: "IDR",
       minimumFractionDigits: hasDecimals ? 2 : 0,
       maximumFractionDigits: hasDecimals ? 2 : 0,
     }).format(price);
