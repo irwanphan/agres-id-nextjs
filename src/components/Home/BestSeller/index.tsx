3ca95cbf03c5401e711ebe69d1bc43ce56cc754f
irwanphan <irwanphan@gmail.com>
2025-07-02 14:38:16 +0700

    Localize various components to Indonesian for improved accessibility
    - Translated Newsletter, BestSeller, and Testimonials components to Indonesian, enhancing user engagement.
    - Updated feature descriptions in the FooterFeature component to reflect localized content.
    - Adjusted layout properties for better responsiveness in the Newsletter component.

--- src/components/Home/BestSeller/index.tsx
+++ src/components/Home/BestSeller/index.tsx
@@ -23,7 +23,7 @@ const BestSeller = async () => {
             href="/shop-without-sidebar"
             className="inline-flex font-medium text-custom-sm py-3 px-7 sm:px-12.5 rounded-lg border-gray-3 border bg-gray-1 text-dark ease-out duration-200 hover:bg-dark hover:text-white hover:border-transparent"
           >
-            View All
+            Lihat Semua
           </Link>
         </div>
       </div>
--- src/components/Home/Hero/FooterFeature.tsx
+++ src/components/Home/Hero/FooterFeature.tsx
@@ -4,33 +4,33 @@ import Image from "next/image";
 const featureData = [
   {
     img: "/images/icons/icon-01.svg",
-    title: "Free Shipping",
-    description: "For all orders $200",
+    title: "Pengiriman Cepat",
+    description: "Tim siap mengirimkan pesanan Anda",
   },
   {
     img: "/images/icons/icon-02.svg",
-    title: "1 & 1 Returns",
-    description: "Cancellation after 1 day",
+    title: "Pengembalian Barang",
+    description: "Barang tidak sesuai bisa dikembalikan",
   },
-  {
-    img: "/images/icons/icon-03.svg",
-    title: "100% Secure Payments",
-    description: "Gurantee secure payments",
-  },
+  // {
+  //   img: "/images/icons/icon-03.svg",
+  //   title: "Pembayaran Aman",
+  //   description: "Pembayaran via partner terpercaya",
+  // },
   {
     img: "/images/icons/icon-04.svg",
-    title: "24/7 Dedicated Support",
-    description: "Anywhere & anytime",
+    title: "Dukungan 24/7",
+    description: "Bisa melalui WhatsApp, Telegram, atau Email",
   },
 ];
 
 const FooterFeature = () => {
   return (
     <section className="pb-[60px]">
-      <div className="max-w-[1060px] w-full mx-auto px-4 sm:px-8 xl:px-0 ">
+      <div className="max-w-[1060px] mx-auto px-4 sm:px-8 xl:px-0 ">
         <div className="flex flex-wrap items-center gap-7.5 xl:gap-12.5">
           {featureData.map((item, key) => (
-            <div className="flex items-center gap-4" key={key}>
+            <div className="flex items-center justify-center gap-4" key={key}>
               <Image src={item.img} alt="icons" width={40} height={41} />
 
               <div>
